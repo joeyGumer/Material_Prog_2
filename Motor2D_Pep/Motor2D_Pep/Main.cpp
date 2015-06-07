feba1178bff029e96426baafaab200562c9f2701
joeyGumer <pep3553@gmail.com>
2015-06-08 00:00:51 +0200

    arreglado

--- Motor2D_Pep/Motor2D_Pep/Main.cpp
+++ Motor2D_Pep/Motor2D_Pep/Main.cpp
@@ -30,43 +30,7 @@ int FibonacciIterative(const unsigned int index)
 
 void main(int argc, char** argv)
 {
-<<<<<<< HEAD
-	Dlist<int> mylist;
 
-	mylist.Add(5);
-	mylist.Add(4);
-	mylist.Add(10);
-	mylist.Add(8);
-	mylist.Add(3);
-
-	mylist.BubbleSort();
-
-=======
-	LOG("%d ", FibonacciIterative(10))
-
-	/*DynArray<int> array1, array2, array3, array4;
-	int r;
-	for (int i = 0; i < 10000; i++)
-	{
-		r = rand() % 10;
-		array1.PushBack(r);
-		array2.PushBack(r);
-		array3.PushBack(r);
-		array4.PushBack(r);
-	}
-	LOG("se ha iterado: %d veces ", array2.BubbleSort());
-	LOG("se ha iterado: %d veces ", array1.BubbleSortOptimized());
-	LOG("se ha iterado: %d veces ", array3.CocktailSort());
-	LOG("se ha iterado: %d veces ", array4.CombSort());
-
-	for (int j = 0; j < 10000; j++)
-	{
-		if (array1[j] != array4[j])
-			LOG("No se ha ordenado correctamente");
-	}
-	
-	return array1.BubbleSort();*/
->>>>>>> origin/master
 }
 
 //factorial (!4 = 24)
